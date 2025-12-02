270c938dcc0f6d2b66296178c2bfa9e2390dcdab
guilhermbc <satelliteex@gmail.com>
2025-12-02 14:12:45 -0300

    fix(app_config): mudança do modelo para gemini-flash

--- backend/webhook_server/agents/agent_transcricao.py
+++ backend/webhook_server/agents/agent_transcricao.py
@@ -21,7 +21,7 @@ persona_message_transcricao = SystemMessage(
 def transcribe_audio_agent(inputs):
     audio_file_path = inputs["video_entrevista"]
     genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-    model_gemini = genai.GenerativeModel("gemini-2.5-flash-preview-05-20")
+    model_gemini = genai.GenerativeModel("gemini-2.5-flash")
     if not os.path.exists(audio_file_path):
         raise FileNotFoundError(f"Arquivo de áudio não encontrado: {audio_file_path}")
     
