240a2d740ff3807575e967c3ec84edf7b44cf57f
guilhermbc <satelliteex@gmail.com>
2026-03-03 15:22:25 -0300

    fix(docker-compose-and-entrypoint.sh): fix do funcionamento container

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "Running migrations..."
+python manage.py makemigrations --noinput
+
 echo "Applying migrations..."
 python manage.py migrate --noinput
 
@@ -10,7 +13,30 @@ echo "Creating superuser..."
 python manage.py createsuperuser --noinput || true
 
 echo "Creating OAuth application..."
-python manage.py createapplication public password --user "$DJANGO_SUPERUSER_USERNAME" --client-id "$OAUTH_CLIENT_ID" || true
+
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+from oauth2_provider.models import Application
+
+User = get_user_model()
+
+username = "$DJANGO_SUPERUSER_USERNAME"
+client_id = "$OAUTH_CLIENT_ID"
+
+user = User.objects.get(username=username)
+
+if not Application.objects.filter(client_id=client_id).exists():
+    Application.objects.create(
+        name="Frontend",
+        user=user,
+        client_id=client_id,
+        client_type=Application.CLIENT_PUBLIC,
+        authorization_grant_type=Application.GRANT_PASSWORD,
+    )
+    print("OAuth application created.")
+else:
+    print("OAuth application already exists.")
+EOF
 
 echo "Starting server..."
-exec gunicorn --bind 0.0.0.0:8000 --workers=2 rai.wsgi:application
+python manage.py runserver 0.0.0.0:8000
